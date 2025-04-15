1cee204d27cbd4f183bf485e142cbcc7ae177968
BSdeployment <142491330+BSdeployment@users.noreply.github.com>
2025-04-15 09:55:21 +0300

    Update portscan.py

--- networks/portscan.py
+++ networks/portscan.py
@@ -1,1 +1,17 @@
+import socket
 
+print("enter ip adress:")
+
+ip = input()
+porst  = [80,3400,3005]
+
+for port in porst:
+    sock =socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    sock.settimeout(1)
+    result = sock.connect_ex((ip,port))
+    if result == 0:
+        print(f"port {port} is open")
+    else:
+         print(f"port {port} is close")
+    sock.close()
+    
